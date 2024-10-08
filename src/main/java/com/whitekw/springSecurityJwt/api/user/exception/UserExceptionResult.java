5f51bb19f75705922392c73b45ff80b77f39b9ec
WhiteKW <53373114+whitekw@users.noreply.github.com>
2024-10-08 17:42:47 +0900

    회원가입 이메일 중복 예외처리

--- src/main/java/com/whitekw/springSecurityJwt/api/user/exception/UserExceptionResult.java
+++ src/main/java/com/whitekw/springSecurityJwt/api/user/exception/UserExceptionResult.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserExceptionResult {
 
-    NOT_EXISTS(HttpStatus.BAD_REQUEST, "U0001", "존재하지 않는 사용자입니다.");
+    NOT_EXISTS(HttpStatus.BAD_REQUEST, "U0001", "존재하지 않는 사용자입니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U0002", "이미 가입된 메일 주소입니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/whitekw/springSecurityJwt/api/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/whitekw/springSecurityJwt/api/user/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package com.whitekw.springSecurityJwt.api.user.service.impl;
 
 import com.whitekw.springSecurityJwt.api.user.dto.UserAddRequestDto;
 import com.whitekw.springSecurityJwt.api.user.entity.User;
+import com.whitekw.springSecurityJwt.api.user.exception.UserException;
+import com.whitekw.springSecurityJwt.api.user.exception.UserExceptionResult;
 import com.whitekw.springSecurityJwt.api.user.repository.UserRepository;
 import com.whitekw.springSecurityJwt.api.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,12 @@ public class UserServiceImpl implements UserService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void addUser(UserAddRequestDto reqDto) {
+
+        Optional<User> existsUser = userRepository.findByEmail(reqDto.getEmail());
+
+        if (!existsUser.isEmpty()) {
+            throw new UserException(UserExceptionResult.DUPLICATE_EMAIL);
+        }
 
         User user = User.of(reqDto);
         user.encryptPassword(bCryptPasswordEncoder);
